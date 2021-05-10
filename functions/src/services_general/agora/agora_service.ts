8c016abd2d7f6f8e4e7dad393e1ef62f43aa3053
mukai154 <mukaiss@outlook.com>
2021-05-10 12:19:35 -0500

    re-arch

--- functions/src/services_general/agora/agora_service.ts
+++ functions/src/services_general/agora/agora_service.ts
@@ -1,27 +1,26 @@
-import * as admin from 'firebase-admin';
 
 const {RtcTokenBuilder, RtcRole} = require('agora-access-token');
-const streamsRef = admin.firestore().collection('webblen_live_streams');
-const agoraDocRef = admin.firestore().collection('app_release_info').doc('agora');
+
 
 export async function generateAgoraToken(data: any){
-    const streamID = data.streamID;
-    const channelName = data.streamTitle;
-    const agoraUID = data.agoraUID;
-    const agoraDoc = await agoraDocRef.get();
-    const appID = agoraDoc.data()!.appID;
-    const appCertificate = agoraDoc.data()!.appCertificate;
-    const role = RtcRole.PUBLISHER; 
+    const channelName = data.channelName;
+    const uid = data.uid;
+    let role;
+    if (data.role === 'publisher'){
+        role = RtcRole.PUBLISHER;
+    } else {
+        role = RtcRole.SUBSCRIBER;
+    }
+    const appID = '60693de17bbe4f2598f9f465d1695de1';
+    const appCertificate = 'a2dc17f3de01494f94b2f5114754dce6';
 
-    const expirationTimeInSeconds = 172800000; //48hr expiration
-
+    const expirationTimeInSeconds = 3600; //5 min expiration
     const currentTimestamp = Math.floor(Date.now() / 1000);
 
     const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
     // Build token with uid
-    const token = RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channelName, agoraUID, role, privilegeExpiredTs);
-    console.log("Token With Integer Number Uid: " + token);    
-    await streamsRef.doc(streamID).update({
-        'agoraToken': token,
-    });
+    const token = RtcTokenBuilder.buildTokenWithAccount(appID, appCertificate, channelName, uid, role, privilegeExpiredTs);
+
+
+    return token;
 }
