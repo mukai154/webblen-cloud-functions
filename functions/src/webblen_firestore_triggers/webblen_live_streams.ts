0d40fe5b4acce3a37eb48c02f1b70db8b283289b
mukai154 <mukaiss@outlook.com>
2021-06-01 11:49:39 -0500

    stream notification fix

--- functions/src/webblen_firestore_triggers/webblen_live_streams.ts
+++ functions/src/webblen_firestore_triggers/webblen_live_streams.ts
@@ -10,9 +10,11 @@ export const createWebblenLiveStreamTrigger = functions.firestore
 	.document("webblen_live_streams/{doc}")
 	.onCreate(async (event) => {
 		const data = event.data();
-		const authorUsername = await webblenUserService.getUsername(data.hostID);
-		const followersToNotify = await webblenUserService.getFollowersToNotify(data.hostID);
-		await webblenLiveStreamService.createNotificationForWebblenLiveStream(data, authorUsername, followersToNotify);
+		if (data.privacy.toLowerCase() != "private"){
+			const authorUsername = await webblenUserService.getUsername(data.hostID);
+			const followersToNotify = await webblenUserService.getFollowersToNotify(data.hostID);
+			await webblenLiveStreamService.createNotificationForWebblenLiveStream(data, authorUsername, followersToNotify);
+		}
 		return algoliaService.saveWebblenLiveStreamToSearchIndex(data);
 	});
 
